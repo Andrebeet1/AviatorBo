1b8617b3a35581588ee88eb8b6b07d7a7e3b464a
andre <andrebeet1@gmail.com>
2025-05-29 13:37:55 +0200

    Update retirer.js

--- bot/handlers/retirer.js
+++ bot/handlers/retirer.js
@@ -2,32 +2,25 @@ const User = require('../../models/User');
 
 async function handleRetirer(bot, msg) {
   const chatId = msg.chat.id;
-  const telegramId = msg.from.id;
+  const telegramId = msg.from.id.toString();
 
   try {
-    const user = await User.findOne({ telegramId });
+    const user = await User.findByTelegramId(telegramId);
     if (!user) {
       return bot.sendMessage(chatId, `❗ Envoie /start pour t’inscrire.`);
     }
 
-    if (!user.enJeu || user.pari <= 0) {
+    if (!user.en_jeu || user.pari <= 0) {
       return bot.sendMessage(chatId, `⛔ Tu n'as pas de pari en cours.`);
     }
 
     const multiplicateur = +(1.5 + Math.random()).toFixed(2);
     const gain = Math.floor(user.pari * multiplicateur);
 
-    user.enJeu = false;
-    user.balance += gain;
-    user.historique.push({
-      date: new Date(),
-      pari: user.pari,
-      multi: multiplicateur,
-      gain
+    await User.finishGame(telegramId, {
+      gain,
+      multi: multiplicateur
     });
-
-    user.pari = 0;
-    await user.save();
 
     bot.sendMessage(chatId, `✅ Retrait réussi à x${multiplicateur}\n💰 Tu gagnes ${gain} F.`);
   } catch (error) {
@@ -37,4 +30,3 @@ async function handleRetirer(bot, msg) {
 }
 
 module.exports = handleRetirer;
-
